8cd7fe81c7f2d6ea0e4f4e0acaf325b2bff65272
Jonathan S. Winther <35611281+jswinther@users.noreply.github.com>
2020-09-23 16:12:44 +0200

    øhøhøh

--- DiscountsConsole/BusinessLogicLayer/ProductsBusinessLogic.cs
+++ DiscountsConsole/BusinessLogicLayer/ProductsBusinessLogic.cs
@@ -19,7 +19,34 @@ namespace DiscountsConsole.BusinessLogicLayer
 
         public void Run(Stack<string> args)
         {
-            Console.WriteLine(DAL.Get().PriceRange(5, 10).SearchByName("Sød").SortByPrice().Print());
+            Console.WriteLine(args.Count);
+            Console.WriteLine(string.Join(' ', args));
+            List<Product> products = DAL.Get();
+            while (args.Count > 0)
+            {
+                var flag = args.Pop().ToUpper();
+                var arg = args.Pop().ToUpper();
+                switch (flag)
+                {
+                    case "-SORT":
+                        if (arg.Equals("NAME")) products = products.SortByName();
+                        else if(arg.Equals("PRICE")) products = products.SortByPrice();
+                        else throw new Exception("Invalid sorting parameter");
+                        break;
+                    case "-NAMESEARCH":
+                        products = products.SearchByName(arg);
+                        break;
+                    case "-PRICERANGE":
+                        var maxprice = int.Parse(args.Pop());
+                        products = products.PriceRange(int.Parse(arg), maxprice);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(products.Print()); 
+            //Console.WriteLine(DAL.Get().PriceRange(5, 10).SearchByName("Sød").SortByPrice().Print());
         }
     }
 }
--- DiscountsConsole/Program.cs
+++ DiscountsConsole/Program.cs
@@ -18,9 +18,12 @@ namespace DiscountsConsole
                 BrandsBusinessLogic brandsBll = new BrandsBusinessLogic(db);
                 SellersBusinessLogic sellersBll = new SellersBusinessLogic(db);
                 string options = $"Options:" +
-                    $"\n\t-Products\n\t\t[-NameSearch \"\\w+\"]\n\t\t[-Sort [Price|Name]]" +
-                    $"\n\t-Brands" +
-                    $"\n\t-Sellers" +
+                    $"\n\t-Products\n\t\t[-NameSearch \"\\w+\"]\n\t\t[-Sort [Price|Name] [Asc|Desc]]" +
+                    $"\n\t\t[-PriceRange [MinPrice, MaxPrice]]\n\t\t[-PriceGreaterThan [double]]" +
+                    $"\n\t\t[-PriceLesserThan [double]]" +
+                    $"\n\t-Brands\n\t\t[-NameSearch \"\\w+\"]\n\t\t[-Sort [Name] [Asc|Desc]]" +
+                    $"\n\t-Sellers\n\t\t[-NameSearch \"\\w+\"]\n\t\t[-Sort [Name] [Asc|Desc]]" +
+                    $"\n\t-Admin\n\t\t[-Add Product [params]]" +
                     $"\n\t-Help" +
                     $"\n\t Press Ctrl+C to exit";
                 Console.WriteLine(options);
