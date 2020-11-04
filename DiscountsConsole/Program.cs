c9f0c197f3401c7988e323bbc7ab30b3b74f3f99
Squidman95 <squidmanse@gmail.com>
2020-11-04 11:23:44 +0100

    Update Program.cs
    Made login methods

--- DiscountsConsole/Program.cs
+++ DiscountsConsole/Program.cs
@@ -15,12 +15,42 @@ namespace DiscountsConsole
     {
         static void Main()
         {
-            //IDatabase db = new InMemoryDatabase();
-            var db = new DiscountsMongoDB();
+            string loginOptions = $"Welcome, please select login method by typing either:" +
+             $"\n\t-User " +
+             $"\n\t-Admin " +
+             $"\n\t Press Ctrl+C to exit";
+            while (true)
+            {
+                Console.WriteLine(loginOptions);
+                var input = Console.ReadLine().ToUpper();
+                switch (input)
+                {
+                    case "-USER":
+                        userLayer();
+                        break;
+                    case "-ADMIN":
+                        Console.WriteLine("Please Write your password");
+                        input = Console.ReadLine().ToUpper();
+                        if(input == "1234")
+                        {
+                            adminLayer();
+                        }
+                        break;
+                    default:
+                        Console.WriteLine($"Invalid args {input}");
+                        break;
+                }
+            }
+        }
 
-            ProductsBusinessLogic productsBll = new ProductsBusinessLogic(new ProductsDAL(db.Products));
-            BusinessLogicProducts<Brand> brandsBll = new BusinessLogicProducts<Brand>(new BrandsDAL(db.Brands));
-            BusinessLogicProducts<Seller> sellersBll = new BusinessLogicProducts<Seller>(new SellerDAL(db.Sellers));
+        private static void adminLayer()
+        {
+            Console.WriteLine("Yo wutup it's ya boy admingold");
+        }
+
+        private static void userLayer ()
+        {
+            bool userLoggedIn = true;
 
             string options = $"Options:" +
                 $"\n\t-Products\n\t\t[-NameSearch \"\\w+\"]\n\t\t[-Sort [Price|Name] [Asc|Desc]]" +
@@ -30,11 +60,19 @@ namespace DiscountsConsole
                 $"\n\t-Sellers\n\t\t[-NameSearch \"\\w+\"]\n\t\t[-Sort [Name] [Asc|Desc]]" +
                 $"\n\t-Admin\n\t\t[-Add Product [params]]" +
                 $"\n\t-Help" +
+                $"\n\t-Logout" +
                 $"\n\t Press Ctrl+C to exit";
             Console.WriteLine(options);
-            while (true)
+
+            //IDatabase db = new InMemoryDatabase();
+            var db = new DiscountsMongoDB();
+
+            ProductsBusinessLogic productsBll = new ProductsBusinessLogic(new ProductsDAL(db.Products));
+            BusinessLogicProducts<Brand> brandsBll = new BusinessLogicProducts<Brand>(new BrandsDAL(db.Brands));
+            BusinessLogicProducts<Seller> sellersBll = new BusinessLogicProducts<Seller>(new SellerDAL(db.Sellers));
+
+            while(userLoggedIn)
             {
-                
                 var input = Console.ReadLine().Split(' ');
                 Array.Reverse(input);
                 Stack<string> args = new Stack<string>(input);
@@ -53,11 +91,15 @@ namespace DiscountsConsole
                     case "-HELP":
                         Console.WriteLine(options);
                         break;
+                    case "-LOGOUT":
+                        userLoggedIn = false;
+                        break;
                     default:
                         Console.WriteLine($"Invalid args {arg}");
                         break;
                 }
             }
         }
+            
     }
 }
